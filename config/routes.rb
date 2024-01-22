517b14ad13c317c70e9a31535fdb3918e2dfba8f
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-01-22 12:11:53 +0000

    header link end

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resources :books, only: [:new, :index, :show, :edit]
-  resources :users, only: [:index, :edit, :edit]
+  resources :users, only: [:index, :edit, :show]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
