60dc79d3ecf7643521650a466018f0087fdab043
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-01-26 14:09:40 +0000

    step3 rspec

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root 'homes#top'
-  get 'homes/about' => "homes#about", as:"about"
+  root 'home#top'
+  get 'home/about' => "home#about", as:"about"
   resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :edit, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
